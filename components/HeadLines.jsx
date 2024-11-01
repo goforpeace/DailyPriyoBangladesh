0a6a2a5df0cd934dfdc042d4139bab30d156c564
Fazle Rifat Anonto <67061375+goforpeace@users.noreply.github.com>
2024-11-02 02:02:50 +0600

    Update HeadLines.jsx

--- components/HeadLines.jsx
+++ components/HeadLines.jsx
@@ -4,50 +4,49 @@ import Marquee from 'react-fast-marquee';
 import Link from 'next/link';
 
 const HeadLines = ({ news }) => {
+  const head = [
+    { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
+    { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
+    { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
+    { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
+    { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
+    { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' }
+  ];
 
-    const head = [
-        { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
-        { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
-        { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
-        { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
-        { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' },
-        { title: 'OnePlus 11r Solar Red with 512 GB internal storage announced' }
-    ];
-
-    return (
-        <div className='bg-white shadow flex flex-wrap'>
-            <div className='flex md:w-[170px] w-full bg-[#dddddd] relative after:absolute after:bg-[#dddddd] after:w-[20px] after:left-[160px] after:skew-x-[20deg] after:top-0 after:bottom-0 after:z-30'>
-                <div className='md:pl-8 pl-4 w-full py-2 flex justify-start items-center gap-x-1'>
-                    <span><LoadingSpinner type='Ripple' colors={['#800000', '#c80000']} size={'30px'} /></span>
-                    <h2 className='text-[#333333] font-semibold text-lg'>Headlines</h2>
-                </div>
-            </div>
-            <div className='flex md:w-[calc(100%-170px)] w-full'>
-                <div className='flex w-full justify-start items-center'>
-                    <Marquee>
-                        {
-                            Object.keys(news).length > 0 &&
-                            Object.keys(news).map((c, i) => (
-                                <React.Fragment key={i}>
-                                    {
-                                        news[c].length > 0 && news[c].map((n, j) => (
-                                            <Link
-                                                key={n.slug} // Use a unique key (slug) for each news item
-                                                className='py-3 block font-semibold hover:text-[#c80000] pr-12 text-sm'
-                                                href={`/news/${n.slug}`}
-                                            >
-                                                {n.title}
-                                            </Link>
-                                        ))
-                                    }
-                                </React.Fragment>
-                            ))
-                        }
-                    </Marquee>
-                </div>
-            </div>
+  return (
+    <div className='bg-white shadow flex'>
+      <div className='flex md:w-[170px] w-full bg-[#dddddd] relative pr-4'>
+        <div className='md:pl-8 pl-4 w-full py-2 flex justify-start items-center gap-x-1'>
+          <span><LoadingSpinner type='Ripple' colors={['#800000', '#c80000']} size={'30px'} /></span>
+          <h2 className='text-[#333333] font-semibold text-lg'>Headlines</h2>
         </div>
-    );
+      </div>
+      <div className='flex-1 pt-4'>
+        <Marquee 
+          speed={50} 
+          gradient={false}
+          className="text-l font-semibold"
+        >
+          {
+            Object.keys(news).length > 0 &&
+            Object.keys(news).map((category, i) => (
+              <React.Fragment key={i}>
+                {news[category].map((newsItem, j) => (
+                  <Link
+                    key={newsItem.slug}
+                    href={`/news/${newsItem.slug}`}
+                    className='px-4 hover:text-[#c80000]'
+                  >
+                    {newsItem.title}
+                  </Link>
+                ))}
+              </React.Fragment>
+            ))
+          }
+        </Marquee>
+      </div>
+    </div>
+  );
 };
 
 export default HeadLines;
